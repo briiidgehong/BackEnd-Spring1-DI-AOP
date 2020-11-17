d81c25348e6f20ee3535a5e73c41521f3819c6f6
cheepiz92 <73451727+mangozzelli@users.noreply.github.com>
2020-11-17 12:46:53 +0900

    @Autowired3 annotaion 위치(기본생성자/오버로딩생성자/setter)와 required option

--- src/spring/di/consoleUI/InlineExamConsole.java
+++ src/spring/di/consoleUI/InlineExamConsole.java
@@ -6,23 +6,44 @@ import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
 
+    // @Autowired(required = false)
+    // required option: 꼭 bean 이 생성 안되어 있어도 됨
+    // 생성되어있으면 DI / 없으면 DI 안함
+
+    // @Qualifier("exam1")
+    // 기본생성자에서 바인딩
+    private Exam exam;
+
     public InlineExamConsole() {
+        System.out.println("기본생성자 바인딩");
     }
 
-    private Exam exam;
+    // @Autowired
+    // @Qualifier("exam1")
+    // 오버로딩 생성자에서 바인딩
+    // public InlineExamConsole(@Qualifier("exam1")Exam exam) {
     public InlineExamConsole(Exam exam) {
+        System.out.println("오버로딩생성자 바인딩");
         this.exam = exam;
     }
 
+
+    // setter 에서 바인딩
     @Autowired
     @Qualifier("exam1")
     @Override
     public void setExam(Exam exam) {
+        System.out.println("setter 바인딩");
         this.exam = exam;
     }
 
     @Override
     public void print() {
+        if(exam==null)
+        {
+            System.out.printf("total is %d avg is %f\n",0,0);
+        }
+
         System.out.printf("total is %d avg is %f\n",exam.total(),exam.avg());
     }
 }
